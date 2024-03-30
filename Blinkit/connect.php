c54a47a3b6e7fb7cf6fdb01721c18d3c69bc8590
Siddhant22497 <133579862+Siddhant22497@users.noreply.github.com>
2024-03-30 12:25:07 +0530

    Add files via upload

---
+++ Blinkit/connect.php
@@ -0,0 +1,17 @@
+<?php
+$firstname = $_POST['first_name'];
+$secondname = $_POST['second_name'];
+$lastname = $_POST['last_name'];
+$CustDOB = $_POST['CustDOB'];
+$age = $_POST['age'];
+$street_number=$_POST['street_number'];
+$landmark = $_POST['"landmark'];
+$city = $_POST['city'];
+$pincode = $_POST['pincode'];
+$state = $_POST['state'];
+$contact = $_POST['contact'];
+?>
+
+
+
+    
---
+++ Blinkit/p1.js
@@ -0,0 +1,3 @@
+var a="6";
+var b=a+10;
+console.log(b);
